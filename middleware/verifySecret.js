180e66d05589d9b96e6068b98733dc94c4f13411
bhavani shanker <bhavanishanker@bhavanis-MacBook-Pro.local>
2024-12-02 11:41:17 +0530

    done2

--- middleware/verifySecret.js
+++ middleware/verifySecret.js
@@ -1,12 +1,14 @@
+
 const authMiddleware = (req, res, next) => {
-    const authKey = "a1b2c3d4e5f67890123456789abcdef"
-    const apiauthkey  = req.headers;
-    if (!apiauthkey) {
+    const authKey = process.env.SHIPPING_SECRET_KEY
+    const shipping_secret_key  = req.headers["shipping_secret_key"];
+    if (!shipping_secret_key) {
         return res.status(403).json({
             "error": "SHIPPING_SECRET_KEY is missing or invalid"
         });
     }
-    if (apiauthkey !== authKey) {
+    console.log(shipping_secret_key , authKey)
+    if (shipping_secret_key != authKey) {
         return res.status(403).json({
             "error": "Failed to authenticate SHIPPING_SECRET_KEY"
         });
